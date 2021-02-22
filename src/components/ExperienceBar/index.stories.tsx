c2df3204fb733c6bca4fd1f1f2d5d3207f9e9653
André <andrevictor50@gmail.com>
2021-02-22 08:55:31 -0300

    First story

--- src/components/ExperienceBar/index.stories.tsx
+++ src/components/ExperienceBar/index.stories.tsx
@@ -1,9 +1,9 @@
-import { Story, Meta } from '@storybook/react/types-6-0';
-import ExperienceBar from './index';
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ExperienceBar from './index'
 
 export default {
   title: 'ExperienceBar',
-  component: ExperienceBar,
-} as Meta;
+  component: ExperienceBar
+} as Meta
 
-export const Basic: Story = () => <ExperienceBar />;
+export const Basic: Story = (args) => <ExperienceBar {...args} />
