279a7ddcef0d4c575eeacf96cb88c445b422f9b1
André Victor <53975579+andrecampll@users.noreply.github.com>
2021-02-26 23:27:37 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import ExperienceBar from 'components/ExperienceBar'
-import { Container } from '../styles/Home'
+import ExperienceBar from 'components/ExperienceBar';
+import { Container } from '../styles/Home';
 
 export default function Home() {
   return (
     <Container>
       <ExperienceBar />
     </Container>
-  )
-}
+  );
+};
